2ee7290184ae26012b9a9d4ba39f27838d395136
tamanugi <tamanugi@gmail.com>
2020-10-06 12:40:40 +0900

    🎉 initial commit

---
+++ lib/ex_at_coder.ex
@@ -0,0 +1,18 @@
+defmodule ExAtCoder do
+  @moduledoc """
+  Documentation for `ExAtCoder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExAtCoder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_at_coder_test.exs
@@ -0,0 +1,8 @@
+defmodule ExAtCoderTest do
+  use ExUnit.Case
+  doctest ExAtCoder
+
+  test "greets the world" do
+    assert ExAtCoder.hello() == :world
+  end
+end
