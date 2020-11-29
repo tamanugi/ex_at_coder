6fb29bac3c0dd389061ca628160d454109ec7e6f
tamanugi <tamanugi@gmail.com>
2020-11-30 00:06:40 +0900

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAtCoder.MixProject do
   def project do
     [
       app: :ex_at_coder,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       description: "mix task for Your AtCoder LIFE✨",
       start_permanent: Mix.env() == :prod,
